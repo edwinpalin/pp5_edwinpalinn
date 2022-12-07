62742495d1b7621dd651002a553d926b3c40589f
edwinpalin <120063550+edwinpalin@users.noreply.github.com>
2022-12-07 22:34:39 +0000

    paso 4

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-with open('gift_costs.txt') as f:
+"""
+Edwin Palin
+22-0178
+Refinando código
+En este ejercicio, se refinará un código dado y se publicará en un repositorio de Github
+"""
+
+
+def costs():
+  with open('gift_costs.txt') as f:
     gift_costs = f.read().split('\n')
-    
-gift_costs = [int(c) for c in gift_costs]  # convierte strings a int
+  gift_costs = [int(c) for c in gift_costs]  # convierte strings a int
+  return gift_costs
 
-total_price = 0
-for cost in gift_costs:
+
+def total(gift_costs):
+  gift_costs = costs()
+  total_price = 0
+  for cost in gift_costs:
     if cost > 1000:
-        total_price += cost * 1.16  # agrega impuestos
+      total_price += cost * 1.16  # agrega impuestos
     else:
-        total_price += cost
+      total_price += cost  # los costos menores a 1000 no se le agrega impuesto
 
-print(total_price)
+  return total_price
+
+
+def main():
+  print(total(costs()))
+  # llama a las dos funciones y luego imprime el resultado
+
+
+main()
